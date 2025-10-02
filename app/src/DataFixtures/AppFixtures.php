b45ba5e1719da47773cb54eab0daeb37d87ee900
Halil <halilxdev@halilxdev-AirM4.local>
2025-10-02 18:13:05 +0200

    Gestion des fixtures : Création de 50 conseils avec un 'text' générique et un 'month' aléatoire entre 1 et 12 qui représentent les mois de l'année au format numérique

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Advice;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,13 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        // Création d'une trantaine de conseils
+        for ($i = 0; $i < 50; $i++) {
+            $advice = new Advice();
+            $advice->setMonth(rand(1,12));
+            $advice->setText('Conseil numéro : ' . $i);
+            $manager->persist($advice);
+        }
         $manager->flush();
     }
 }
